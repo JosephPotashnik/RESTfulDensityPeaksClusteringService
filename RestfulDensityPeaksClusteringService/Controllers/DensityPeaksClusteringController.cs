76d0675c302226d0c2590e2286fed3e3571f8cf0
Joseph Potashnik <joseph.potashnik@gmail.com>
2018-10-25 16:10:04 +0100

    RESTful Web Service for Density Based Clustering

---
+++ RestfulDensityPeaksClusteringService/Controllers/DensityPeaksClusteringController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DensityPeaksClustering;
+using RestfulDensityPeaksClusteringService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulDensityPeaksClusteringService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cluster")]
+    public class DensityPeaksClusteringController : Controller
+    {
+        //Functions From Data (Distance is computed here).
+
+        [HttpPost]
+        [Route("DPClusteringWithMultiManifold/{distanceType}")]
+        public int[] DPClusteringWithMultiManifold([FromBody] DataMatrix<float> m,
+            DistanceFunctionType distanceType = DistanceFunctionType.EuclideanDistance)
+        {
+            return DensityPeaksClusteringAlgorithms.MultiManifold(m.Matrix, 0, 0, distanceType);
+        }
+
+        [HttpPost]
+        [Route("DPClusteringWithKNN/{distanceType}")]
+        public int[] DPClusteringWithKNN([FromBody] DataMatrix<float> m,
+            DistanceFunctionType distanceType = DistanceFunctionType.EuclideanDistance)
+        {
+            return DensityPeaksClusteringAlgorithms.KNN(m.Matrix, distanceType);
+        }
+
+
+        [HttpPost]
+        [Route("DPClustering/{cutoffDistance}/{distanceType}")]
+        public int[] DPClustering([FromBody] DataMatrix<float> m, double cutoffDistance,
+            DistanceFunctionType distanceType = DistanceFunctionType.EuclideanDistance)
+        {
+            return DensityPeaksClusteringAlgorithms.DPClustering(m.Matrix, cutoffDistance, distanceType);
+        }
+
+
+        //Functions From Distance Matrix:
+
+        [HttpPost]
+        [Route("DPClusteringFromDistanceMatrix/{cutoffDistance}")]
+        public int[] DPClusteringFromDistanceMatrix([FromBody] DistanceMatrix dMatrix, double cutoffDistance)
+        {
+            return DensityPeaksClusteringAlgorithms.DPClusteringFromDistanceMatrix(dMatrix, cutoffDistance);
+
+        }
+
+        [HttpPost]
+        [Route("DPClusteringWithKNNFromDistanceMatrix")]
+        public int[] DPClusteringWithKNNFromDistanceMatrix([FromBody] DistanceMatrix dMatrix)
+        {
+            return DensityPeaksClusteringAlgorithms.DPClusteringWithKNNFromDistanceMatrix(dMatrix);
+        }
+
+        [HttpPost]
+        [Route("DPClusteringWithMultiManifoldFromDistanceMatrix")]
+        public int[] DPClusteringWithMultiManifoldFromDistanceMatrix([FromBody] DistanceMatrix dMatrix)
+        {
+            return DensityPeaksClusteringAlgorithms.DPClusteringWithMultiManifoldFromDistanceMatrix(dMatrix);
+        }
+    } 
+}
---
+++ RestfulDensityPeaksClusteringService/Models/DataMatrix.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace RestfulDensityPeaksClusteringService.Models
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class DataMatrix<T>
+    {
+        public DataMatrix(int numberOfDimensions, int numberOfSamples)
+        {
+            Matrix = new T[numberOfSamples][];
+
+            for (var i = 0; i < numberOfSamples; i++)
+                Matrix[i] = new T[numberOfDimensions];
+        }
+
+        [JsonProperty] public T[][] Matrix { get; set; }
+
+        public int NumberOfSamples => Matrix?.Length ?? 0;
+        public int DimensionOfSamples => Matrix[0]?.Length ?? 0;
+
+        public T this[int i, int j]
+        {
+            get => Matrix[i][j];
+            set => Matrix[i][j] = value;
+        }
+
+        //single indexer = returns the i-th sample.
+        public T[] this[int i]
+        {
+            get => Matrix[i];
+            set => Matrix[i] = value;
+        }
+    }
+}
